32ce68ea1ff66aac11e4fdf78ec64ec3ba4d90a1
Daniel Silva <diielsilvasousa@gmail.com>
2024-12-10 11:22:02 -0300

    chore: finishing main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject, Signal} from '@angular/core';
+import {MatchService} from './services/match.service';
+import {Match} from './models/match';
+import {ChooseOptionComponent} from './components/choose-option/choose-option.component';
+import {BoardComponent} from './components/board/board.component';
+import {ResetComponent} from './components/reset/reset.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    ChooseOptionComponent,
+    BoardComponent,
+    ResetComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  private matchService: MatchService = inject(MatchService);
+  protected matchState: Signal<Match> = this.matchService.matchState;
+
+  protected choose(option: number): void {
+    this.matchService.chosen(option);
+  }
+
+  protected play(indexes: number[]): void {
+    this.matchService.play(indexes[0], indexes[1]);
+  }
+
+  protected restart(): void {
+    this.matchService.reset();
+  }
+
 }
